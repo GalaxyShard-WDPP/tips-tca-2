2eee5544a40187c6dc1bc42e725dcc3b5580ce2a
GalaxyShard-WDPP <524dadragna@frhsd.com>
2023-01-25 16:28:17 +0000

    full implemention

--- index.js
+++ index.js
@@ -9,18 +9,43 @@ function tipPercent(quality) {
         return;
     }
 }
-function billTotal(subtotal, serviceQuality) {
-    return subtotal + subtotal*tipPercent(serviceQuality);
+function applyTip(cost, serviceQuality) {
+    return cost + cost*tipPercent(serviceQuality);
 }
-function splitCost(partySize, subtotal, serviceQuality) {
-    return billTotal(subtotal, serviceQuality) / partySize;
+function splitCost(partySize, cost, serviceQuality) {
+    return applyTip(cost, serviceQuality) / partySize;
 }
 
-const subtotal = 10.98;
-const quality = "good";
-const partySize = 10;
 
-console.log(`subtotal: ${subtotal}, ${quality} service`);
-console.log(`total: ${billTotal(subtotal, quality)}, tipping ${tipPercent(quality)*100}%`);
-console.log(`cost per person (group of ${partySize}): ${splitCost(partySize, subtotal, quality)}`);
+const costInput = document.getElementById("cost");
+const partySizeInput = document.getElementById("party-size");
+const serviceQualityInput = document.getElementById("service-quality");
+const output = document.getElementById("output");
+const tipPercentOutput = document.getElementById("tip-percent");
 
+partySizeInput.value = 1;
+costInput.value = 0;
+
+function updateDisplay() {
+    const quality = serviceQualityInput.value;
+    const percent = tipPercent(quality)*100; // convert from 0-1 percentage to 0-100 percentage
+    tipPercentOutput.textContent = percent + "%";
+
+    const partySize = parseInt(partySizeInput.value, 10);
+    const cost = parseFloat(costInput.value);
+
+    const total = splitCost(partySize, cost, quality);
+    output.textContent = total.toFixed(2); // converts 'total' to a string with 2 decimal places
+}
+updateDisplay();
+serviceQualityInput.addEventListener("change", _ => { updateDisplay(); });
+partySizeInput.addEventListener("change", _ => {
+    const value = parseInt(partySizeInput.value, 10); // validate input
+    partySizeInput.value = value > 0 ? value : 1;
+    updateDisplay();
+});
+costInput.addEventListener("change", _ => {
+    const value = parseFloat(costInput.value); // validate input
+    costInput.value = value > 0 ? value : 1;
+    updateDisplay();
+});
